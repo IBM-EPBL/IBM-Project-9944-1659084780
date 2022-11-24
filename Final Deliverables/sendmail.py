72018a045ca50fbe1700e56b58e386a15d37e23d
Manideep456 <113247933+Manideep456@users.noreply.github.com>
2022-11-24 17:47:51 +0530

    Update sendmail.py

--- Final Deliverables/sendmail.py
+++ Final Deliverables/sendmail.py
@@ -15,13 +15,13 @@ def sendmail(TEXT,email):
     print("sorry we cant process your candidature")
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("il.pradeepthi@gmail.com", "oms@1Ram")
+    s.login("manideepnlr@gmail.com", "oms1Ram")
     message  = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-    s.sendmail("il.pradeepthi@gmail.com", email, message)
+    s.sendmail("manideepnlr@gmail.com", email, message)
     s.quit()
 def sendgridmail(user,TEXT):
     sg = sendgrid.SendGridAPIClient('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')
-    from_email = Email("sandeep@thesmartbridge.com")  # Change to your verified sender
+    from_email = Email("AC19UCS057@smartinternz.com")  # Change to your verified sender
     to_email = To(user)  # Change to your recipient
     subject = "Sending with SendGrid is Fun"
     content = Content("text/plain",TEXT)
